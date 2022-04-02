b54f579a1b0946add602634cd685656885baf4e1
itay.shaked <itay.shaked@ridewithvia.com>
2022-04-02 11:38:47 +0300

    redsign 1

--- camera_flask_app.py
+++ camera_flask_app.py
@@ -100,8 +100,8 @@ def gen_frames():  # generate frame by frame from camera
 @app.route('/')
 def index():
     return render_template('index.html')
-    
-    
+
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
